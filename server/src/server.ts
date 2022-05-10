58ab4e20dc8b1f9e0d3219f9ef461a10500ecbdb
Amaury Martin <amaury.conde@hotmail.com>
2022-05-10 01:57:35 -0300

    build(server): add cors dependency

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 const port = process.env.PORT || 3001;
 
 const app = express();
+app.use(cors());
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server is running on PORT ${port}`));
