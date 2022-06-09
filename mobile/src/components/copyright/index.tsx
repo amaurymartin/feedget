217ef69fa7ecc7d4c423c87ea127fba936ebd207
Amaury Martin <amaury.conde@hotmail.com>
2022-06-09 00:58:51 -0300

    feat(mobile): create copyright component

---
+++ mobile/src/components/copyright/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+import styles from "./styles";
+
+export default function Copyright() {
+  return (
+    <View>
+      <Text style={styles.text}>Done with ♥ by Rocketseat</Text>
+    </View>
+  );
+}
---
+++ mobile/src/components/copyright/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+import theme from "../../theme";
+
+export default StyleSheet.create({
+  text: {
+    color: theme.colors.text_secondary,
+    fontFamily: theme.fonts.medium,
+    fontSize: 12,
+  },
+});
