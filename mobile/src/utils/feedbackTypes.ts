6df00da0f65af21cbd6f39a2c1574f20485fffa5
Amaury Martin <amaury.conde@hotmail.com>
2022-06-27 23:56:30 -0300

    feat(mobile): add feedbackTypes

---
+++ mobile/src/utils/feedbackTypes.ts
@@ -0,0 +1,16 @@
+/* eslint-disable global-require */
+
+export default {
+  BUG: {
+    title: "Bug",
+    image: require("../assets/bug.png"),
+  },
+  IDEA: {
+    title: "Idea",
+    image: require("../assets/idea.png"),
+  },
+  OTHER: {
+    title: "Other",
+    image: require("../assets/thought.png"),
+  },
+};
