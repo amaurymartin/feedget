afcda37fa9b84db123713ee28aba945511de3866
Amaury Martin <amaury.conde@hotmail.com>
2022-07-06 01:52:49 -0300

    feat(mobile): use expo-splash-screen instead of expo-app-loading

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,9 @@
-import AppLoading from "expo-app-loading";
-import { StatusBar } from "expo-status-bar";
-import {
-  useFonts,
-  Inter_400Regular,
-  Inter_500Medium,
-} from "@expo-google-fonts/inter";
+import { useCallback, useEffect, useState } from "react";
 import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 import "react-native-gesture-handler";
 
 import Widget from "./src/components/widget";
@@ -13,12 +11,32 @@ import Widget from "./src/components/widget";
 import theme from "./src/theme";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_500Medium });
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  if (!fontsLoaded) return <AppLoading />;
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({ Inter_400Regular, Inter_500Medium });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) await SplashScreen.hideAsync();
+  }, [appIsReady]);
+
+  if (!appIsReady) return null;
 
   return (
     <View
+      onLayout={onLayoutRootView}
       style={{
         flex: 1,
         backgroundColor: theme.colors.background,
